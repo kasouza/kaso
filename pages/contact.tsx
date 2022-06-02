0c92e4c20236a0d667589cdcd7d80e57f48fd4f1
kasouza <kaua.scmm@gmail.com>
2022-06-02 20:49:58 -0300

    Unscaped apostrophe oopsie

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
             <section className="flex flex-col gap-12 w-11/12 md:w-4/5 lg:w-3/5 mb-16">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-4xl">Contact</h1>
-                    <p>If you want to talk to me, just fill in the field bellow and I'll respond as soon as possible!</p>
+                    <p>If you want to talk to me, just fill in the field bellow and I&apos;ll respond as soon as possible!</p>
                 </div>
 
                 <Form action="/api/saske" inputs={[
